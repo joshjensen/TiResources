a66df88f3b6838a2ae1fdcc02db0a753d9365272
Josh Jensen <josh@pixelflavor.com>
2013-01-28 09:25:05 -0500

    Added Sample OAuth1.0

---
+++ OAuth1.0/app.js
@@ -0,0 +1,24 @@
+var oAuthAdapter = require('/lib/oauth/oauth').create({
+	service: 'tableproject1',
+	consumerKey: '-- YOUR CONSUMER KEY HERE --',
+	consumerSecret: '-- YOUR CONSUMER SECRET HERE --',
+	signatureMethod: '-- YOU SIGNATURE METHOD HERE -- ', // ex. PLAINTEXT, HMAC-SHA1
+	requestTokenUrl: 'http://example.com/api/v1/token/request',
+	accessTokenUrl: 'http://example.com/api/v1/token/access',
+	authorizeUrl: 'http://example.com/account/authorize?oauth_token=',
+	endpointUrl: 'http://example.com/api/v1/',
+	callbackUrl: 'http://yoursite.com/callbacks/success/'
+});
+
+
+oAuthAdapter.makeRequest({
+    apiPath: 'list/all/people',
+    onSuccess: function(evt) {
+        if (evt.success) {
+            console.log(evt.responseText);
+        }
+    },
+    onError: function(evt) {
+        console.log(JSON.stringify(evt));
+    }
+});
